701950841c23fa2d9de79bd6c815ff8c0644860d
yvars <jarkin.123789@gmail.com>
2017-09-13 14:42:22 +0300

    Add DB

--- src/main/java/com/softserve/config/SpringContextLoaderListener.java
+++ src/main/java/com/softserve/config/SpringContextLoaderListener.java
@@ -1,18 +1,22 @@
 package com.softserve.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-@RequiredArgsConstructor
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
 public class SpringContextLoaderListener implements ServletContextListener {
 
     private final AnnotationConfigWebApplicationContext context;
+    
+    
+    public SpringContextLoaderListener(AnnotationConfigWebApplicationContext context) {
+		super();
+		this.context = context;
+	}
 
-    @Override
+	@Override
     public void contextInitialized(ServletContextEvent sce) {
         sce.getServletContext().setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
         context.setServletContext(sce.getServletContext());
